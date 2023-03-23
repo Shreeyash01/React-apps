3982bebc075dd86e67bead4715ac3746f3cdd452
SHREEYASH MAHAJAN <103109932+Shreeyash01@users.noreply.github.com>
2023-03-23 23:10:45 +0530

    Add files via upload

---
+++ Weather App/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Weather from "./components/Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
